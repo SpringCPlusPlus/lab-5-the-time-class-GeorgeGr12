ef16648de367a3b065c4ddba85471f495bebab13
GeorgeGr12 <georgegrammatosis@gmail.com>
2025-04-01 23:47:06 -0400

    LAB5: George Grammatosis - Added operators overloading for Time class

--- src/Time.cpp
+++ src/Time.cpp
@@ -5,29 +5,49 @@ using namespace std;
 // Overload the + operator
 Time Time::operator+(const Time &other) const
 {   
+    int totalMinutes = minutes + other.getMinutes();
+    int newHours = totalMinutes / 60;
+    int newMinutes = totalMinutes % 60;
+    int totalHours = (hours + other.getHours()) + newHours;
+    return Time(totalHours, newMinutes);
 }
 
 // Overload the += operator
 Time& Time::operator+=(const Time &other)
 {   
+    *this = *this + other;
+    return *this;
 }
 
 // Overload the < operator
 bool Time::operator<(const Time &other) const
-{
+{   
+
+    if (hours < other.getHours()) return true;
+    else if (hours == other.getHours()) {
+        if (minutes < other.getMinutes()) return true;
+    }
+    else return false;
 }
 
 // Overload the > operator
 bool Time::operator>(const Time &other) const
 {
+    if (hours > other.getHours()) return true;
+    else if (hours == other.getHours()) {
+        if (minutes > other.getMinutes()) return true;
+    }
+    else return false;
 }
 
 // Overload the == operator
 bool Time::operator==(const Time &other) const
 {
+    return (this->hours == other.getHours()) && (this->minutes == other.getMinutes());
 }
 
 // Overload the friend function operator<<
 ostream &operator<<(ostream &os, const Time &tm)
 {
+    return os << tm.getHours() << ":" << tm.getMinutes();
 }
